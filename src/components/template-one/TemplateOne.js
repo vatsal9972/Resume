6d9a65c804040bf6106a597513e818ad92387a26
umang balsara <umangbalsara@knovator.in>
2023-07-23 20:10:03 +0530

    Project Setup Home Page Created Resume Form Created

---
+++ src/components/template-one/TemplateOne.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TemplateOne.css";
+
+const TemplateOne = () => {
+  return (
+    <div>
+      TemplateOne buyu hughuyhubjhb jhbjvjvjv mjv jgvjvjhvjvuyjgvjyugyuguygy
+    </div>
+  );
+};
+
+export default TemplateOne;
