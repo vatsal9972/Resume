5602b996bb68ee401dcbe277517517cd0e6d30a3
Tanvi Balsara <tanvibalsara18@gmail.com>
2023-09-23 20:39:01 +0530

    TemplateOne photo addition

--- src/components/profile-select-file/ProfileSelectFile.js
+++ src/components/profile-select-file/ProfileSelectFile.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileSelectFile.css";
 
-const ProfileSelectFile = ({ setResumeDetails, name }) => {
-  const onSelectProfile = (e) => {
-    const file = e.target.files[0];
-    setResumeDetails((prev) => ({ ...prev, [name]: file }));
-  };
-  return (
-    <input
-      placeholder="Select your profile picture"
-      type="file"
-      onChange={onSelectProfile}
-    />
-  );
-};
+function ProfileSelectFile({ name, setResumeDetails }) {
+  const [selectedImageSrc, setSelectedImageSrc] = useState("");
+  const [imageContainerDisplay, setImageContainerDisplay] = useState("none");
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setSelectedImageSrc(e.target.result);
+        setImageContainerDisplay("block");
+        setResumeDetails((prev) => ({ ...prev, [name]: e.target.result }));
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          onChange={handleImageChange}
+        />
+      </form>
+      <div style={{ display: imageContainerDisplay }} id="imageContainer">
+        <img
+          id="selectedImage"
+          src={selectedImageSrc}
+          alt="Selected Image"
+          style={{ width: 70 }}
+        />
+      </div>
+    </div>
+  );
+}
 export default ProfileSelectFile;
